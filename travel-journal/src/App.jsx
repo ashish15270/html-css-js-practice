f5db02bb2189811b3965b5ac9d18b62a034b2b95
ashish kumar <a.ashish01kumar@gmail.com>
2025-09-13 22:55:11 +0530

    travel-journal

---
+++ travel-journal/src/App.jsx
@@ -0,0 +1,12 @@
+import { hydrateRoot } from "react-dom/client";
+import Header from "./Header"
+import Entry from "./entry"
+
+export default function App() {
+    
+    return <>
+    <Header />
+    <Entry />
+    </>
+    
+}
