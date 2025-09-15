d354319c9a1240f3926bcdf903659158337efb71
ashish kumar <a.ashish01kumar@gmail.com>
2025-09-15 20:16:59 +0530

    chef-claude

--- chef-claude/src/Body.jsx
+++ chef-claude/src/Body.jsx
@@ -1,12 +1,12 @@
-import { useImperativeHandle } from "react";
-
 export default function Body() {
     return (
         <main>
+            <form className="add-ingredient-form" action="">
              <input type="text" 
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"/>
     <button>Add ingredients</button>
+    </form>
         </main>
    
     )
