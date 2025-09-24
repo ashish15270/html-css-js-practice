8803f50be3fb59192dc2a7b9845d3319e910ee8c
ashish kumar <a.ashish01kumar@gmail.com>
2025-09-24 10:09:08 +0530

    added claude display ingredients logic

--- chef-claude/src/Body.jsx
+++ chef-claude/src/Body.jsx
@@ -1,12 +1,35 @@
+import React from "react"
+
 export default function Body() {
+
+    const [ingredients, updateIngredients]=React.useState([])
+
+    const listIngredients=ingredients.map(ing => 
+        <ul>
+            <li key={ing}>{ing}</li>
+        </ul>
+        )
+
+    function handleIngredient(event){
+        event.preventDefault()
+        const formData=new FormData(event.currentTarget)
+        const ingredient=formData.get("ingredient")
+        console.log(ingredient)
+        updateIngredients(ingredients => [...ingredients, ingredient])
+    }
+
+
+
     return (
         <main>
-            <form className="add-ingredient-form" action="">
+            <form onSubmit={handleIngredient} className="add-ingredient-form" action="">
              <input type="text" 
+                    name="ingredient"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"/>
     <button>Add ingredients</button>
     </form>
+    {listIngredients}
         </main>
    
     )
