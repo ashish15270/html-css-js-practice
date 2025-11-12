6e1b77f79c4d291f143a1cbcbc6c4aadcab7a94e
ashish kumar <a.ashish01kumar@gmail.com>
2025-11-12 22:44:28 +0530

    commit-message

--- meme-generator/components/Main.jsx
+++ meme-generator/components/Main.jsx
@@ -1,22 +1,40 @@
+import { useState } from "react"
+
 export default function Main() {
+
+    const [meme, memeSetter]=useState({
+        topText: "One does not simply",
+        bottomText: "walk into mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleEvent(event) {
+        const { value, name }=event.currentTarget
+
+        memeSetter(prev => ({
+            ...prev,
+            [name]:value
+        }))
+    }
+
     return (
     <main>
     <div className="form">
 
     <label htmlFor="">Top Text
-    <input type="text" placeholder="One does not simply" name="toptext"></input>
+    <input type="text" placeholder="One does not simply" name="topText" onChange={handleEvent} ></input>
     </label>
 
 
     <label htmlFor="">Top Text
-    <input type="text" placeholder="walk into mordor" name="bottomtext"></input>
+    <input type="text" placeholder="walk into mordor" name="bottomText" onChange={handleEvent}></input>
     </label>
     <button>Get a meme</button>
 
     <div className="meme">
-    <img src="http://i.imgflip.com/1bij.jpg" />
-    <span className="top">One does not simply</span>
-    <span className="bottom">Walk into Mordor</span>
+    <img src={meme.imageUrl} />
+    <span className="top">{meme.topText}</span>
+    <span className="bottom">{meme.bottomText}</span>
     </div>
 
     </div>
