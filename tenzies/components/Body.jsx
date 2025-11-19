8dc539308978a528c8b492af1726e8efad3c74a3
ashish kumar <a.ashish01kumar@gmail.com>
2025-11-19 10:34:08 +0530

    commit-message

--- tenzies/components/Body.jsx
+++ tenzies/components/Body.jsx
@@ -1,27 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import Die from "./Die"
 
 export default function Body(){
     
 
-const [diceNums, diceNumArrSet]=useState(getdiceNums())
-let endgame=false
-function endGame(){
-    let win=0
-    const winNum=diceNums[0]['value']
-    for (let index = 0; index < 10; index++) {
-        if (diceNums[index]['value']===winNum) {
-            win=win+1
-        }           
-    }
-    console.log(diceNums)
-    if (win===10){
-        endgame=true
-    }
-}
+const [diceNums, diceNumArrSet]=useState(()=>getdiceNums())
 
-endGame()
+const endgame=diceNums.every(dice=> dice.isHeld) && diceNums.every(dice => diceNums[0].value===dice.value)
+
+const newGameButton=useRef(null)
+
+// function endGame(){
+//     let win=0
+//     const winNum=diceNums[0]['value']
+//     for (let index = 0; index < 10; index++) {
+//         if (diceNums[index]['value']===winNum) {
+//             win=win+1
+//         }           
+//     }
+//     console.log(diceNums)
+//     if (win===10){
+//         endgame=true
+//     }
+// }
+
+useEffect(()=>{
+    newGameButton.current.focus()
+}, [endgame])
 
 
 function getdiceNums(){
@@ -32,7 +38,6 @@ function getdiceNums(){
     tempObj={value:EachDiceNum,isHeld:false, id:index}   
     tempArray.push(tempObj)
     }
-    // console.log(tempArray)
     return(tempArray)
     }
 
@@ -48,11 +53,15 @@ function getdiceNums(){
 
 
     function rollUnheld(){
+        if (!endgame){
         diceNumArrSet(
             prev => 
                 prev.map( die => die.isHeld ? die : 
                     {...die, value: Math.floor(Math.random()*6)+1}
                  ))
+                }
+            else {
+                diceNumArrSet(getdiceNums())}
     }
 
     const diceEle=diceNums.map(dieObj =>
@@ -62,13 +71,12 @@ function getdiceNums(){
     
     return (
         <main className="main-container">
-         {!endgame && <section className="dies">
+         <section className="dies">
             {diceEle}
-            </section>}
-            {endgame && <section className="dies">
-                 <h1> You won!</h1> 
-                 </section> }
-            <button onClick={rollUnheld}>Roll the dice!</button>
+            </section>
+            <button onClick={rollUnheld} ref={newGameButton}>
+                {endgame ? "Restart": "Roll the dice!"}
+                </button>
         </main>
     )
 }
